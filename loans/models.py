2805a581393452747c63ff6ebb55b34d22db0f64
Aryan Sanvee Vijayan <aryansanvee2006@gmail.com>
2025-10-31 02:39:26 +0000

    Another bug fix

--- loans/models.py
+++ loans/models.py
@@ -10,16 +10,16 @@ class Book (models.Model):
     def __str__(self):
         return(f'{self.authors} ({self.publication_date}) "{self.title}" ISBN {self.isbn}.' )
     
-class Member(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField()
+# class Member(models.Model):
+#     first_name = models.CharField(max_length=100)
+#     last_name = models.CharField(max_length=100)
+#     email = models.EmailField()
 
-    def __str__(self):
-        return(f"Member {self.id}: {self.last_name}, {self.first_name} <{self.email}>")
+#     def __str__(self):
+#         return(f"Member {self.id}: {self.last_name}, {self.first_name} <{self.email}>")
     
-class Loan(models.Model):
-    member = models.ForeignKey(Member, on_delete=models.SET_NULL)
-    book = models.ForeignKey(Book, on_delete=models.SET_NULL)
-    start_at = models.DateField()
-    end_at = models.DateField()
+# class Loan(models.Model):
+#     member = models.ForeignKey(Member, on_delete=models.SET_NULL)
+#     book = models.ForeignKey(Book, on_delete=models.SET_NULL)
+#     start_at = models.DateField()
+#     end_at = models.DateField()
